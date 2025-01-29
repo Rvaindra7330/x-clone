b592aa24f47671f0d28b4640ee66e276085b1136
Ravindra7330 <ravindarravi87@gmail.com>
2025-01-29 20:57:27 +0530

    added signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,35 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { Hono } from 'hono'
+import { sign } from 'hono/jwt';
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string,
+    JWT_SECRET:string
+  }
+}>();
+
+
+app.post('/signup', async(c) => {
+  
+  const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+  const body= await c.req.json();
+
+  const user= await prisma.muggle.create({
+    data:{
+      email:body.email,
+      password:body.password
+    }
+  })
+  const jwt=await sign({id:user.id},c.env.JWT_SECRET);
+  return c.json({jwt})
+})
 
 app.post('/signin', (c) => {
-  return c.text('Hello Hono!')
-})
-app.post('/signup', (c) => {
   return c.text('Hello Hono!')
 })
 app.post('/blog', (c) => {
