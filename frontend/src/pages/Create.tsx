fbb8df2c267f1a2bb0a8791ed21b2c3df1701726
Ravindra7330 <ravindarravi87@gmail.com>
2025-02-10 15:43:45 +0530

    post api

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -3,20 +3,25 @@ import { BACKEND_URL } from "./config";
 import { useEffect, useState } from "react";
 
 export const Create=({ isOpen, setIsOpen }:any)=>{
-  const [post,setPost]=useState('');
-  useEffect(()=>{
-    const token=localStorage.getItem('token')
-    const parsedToken=JSON.parse(token || "")
-    async function sendPost(){
-        await axios.post(`${BACKEND_URL}/post`,post,{
-        headers:{
-          Authorization:parsedToken
-        }
-      })
-    }
-  },[post])
+ 
  
     if (!isOpen) return null;
+    const [post,setPost]=useState({
+      content:""
+    });
+      const token=localStorage.getItem('token')
+      const parsedToken=JSON.parse(token || "")
+       const sendPost=async()=>{
+          const res=await axios.post(`${BACKEND_URL}/post`,post,{
+          headers:{
+            Authorization:parsedToken
+          }
+        })
+        console.log(res.data)
+        setIsOpen(false)
+      }
+      
+    
     return <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
     <div className="relative p-6 w-full max-w-2xl bg-gray-900 rounded-lg shadow-lg">
       
@@ -32,14 +37,20 @@ export const Create=({ isOpen, setIsOpen }:any)=>{
 
       
       <div className="mt-4">
-        <textarea onChange={(e)=>
-          setPost(e.target.value)
+        <textarea onChange={(e)=>{
+          setPost(c=>({
+            ...c,
+            content:e.target.value
+          }))
+        }
+         
+        
         } className="w-full p-2  bg-gray-900 caret-white focus:outline-none text-white" placeholder="What is happening?!" />
       </div>
-       
+      
       
       <div className="flex justify-end mt-4 space-x-2">
-      <button onClick={()=>setIsOpen(false)} type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Cancel</button>
+      <button onClick={()=>setIsOpen(false)}  type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Cancel</button>
 
       <button onClick={sendPost} type="button" className="text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Post</button>
       </div>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,15 +9,15 @@ export const Home=()=>{
       <div className="fixed top-0 left-0 h-full w-1/4"><SideBar></SideBar></div>
       
     </div>
-    <div className="col-span-9 ml-1/4  h-screen  divide-x divide-slate-800 overflow-auto flex flex-row">
-    <div className="w-2/3 p-4">
+   
+    <div className="col-span-5 ml-1/4  h-screen">
     <Posts></Posts>
     </div>
-    <div className="w-1/3 p-4">
+    <div className="col-span-4 ml-1/4  h-screen">
       <SearchBar></SearchBar>
       
     </div>
-    </div>
+    
     
     
     
